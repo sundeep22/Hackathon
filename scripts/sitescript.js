68058735437d6739d74a1b9558264f9db73fe315
Sundeep Suram <susuram@microsoft.com>
2017-07-26 11:35:03 +0530

    added sliding.

--- scripts/sitescript.js
+++ scripts/sitescript.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
         document.querySelector("video").addEventListener("timeupdate", function () {
             var currentTime = this.currentTime;
             $.each(data, function (index, element) {
-                var currentAd = "ad" + element.id;
+                var currentAd = "slideDiv" + element.id;
                 if (currentTime > element.startTime && currentTime < element.endTime) {
                     if ($("#" + currentAd).length == 0) {
-                        var html = '<div id="' + currentAd + '" class="overlay2" onclick=window.open("'+ element.storeUrl +'") style="display:inline; cursor:pointer; background-image:url('+ element.iconUrl +')"></div>';
+                        var html = '<div id="slideDiv' + element.id  + '" style="display:none"> <div id="' + currentAd + '" class="overlay2" onclick=window.open("'+ element.storeUrl +'") style="display:inline; cursor:pointer; background-image:url('+ element.iconUrl +')"></div></div>';
                         $(html).appendTo($("#adContainer")).slideDown("slow"); 
                     }
                 }
                 else {
-                    $("#" + currentAd).remove();
+                    $("#" + currentAd).slideUp("slow", function(){ $(this).remove(); });
                 }
             });
         });
